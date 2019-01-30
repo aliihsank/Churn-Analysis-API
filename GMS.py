b4f63c897dd3a2052f40155f3c4d7de2e87c0517
Ali İhsan Karabal <aliihsankarabal@eskisehir.edu.tr>
2019-01-30 21:21:51 +0300

    Update GMS.py

--- GMS.py
+++ GMS.py
@@ -108,7 +108,7 @@ class GMS:
         
         '''Assign columns'''
         self.X = self.data_frame.iloc[:, (self.categoricalcolumns + self.numericalcolumns)].values
-        self.y = self.data_frame.iloc[:, [self.target]].values
+        self.y = self.data_frame.iloc[:, self.target].values
         print(self.y)
 
         '''Encode categorical vars '''
