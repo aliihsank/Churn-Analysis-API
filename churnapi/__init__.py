6599339f65cc00b2addb92b6caeddc8539becdcd
Ali İhsan Karabal <aliihsankarabal@eskisehir.edu.tr>
2019-02-08 15:24:02 +0300

    Create __init__.py

---
+++ churnapi/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+CORS(app)
+
+import churnapi.resources
