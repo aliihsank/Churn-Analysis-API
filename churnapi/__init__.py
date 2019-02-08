e362337f146055725cd40e29adf73d97605bd708
Ali İhsan Karabal <aliihsankarabal@eskisehir.edu.tr>
2019-02-08 16:57:33 +0300

    Database conString encapsulated

--- churnapi/__init__.py
+++ churnapi/__init__.py
@@ -1,8 +1,11 @@
+import os
 from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.debug = False
+MONGO_URL = os.environ.get('MONGO_URL')
 api = Api(app)
 CORS(app)
 
